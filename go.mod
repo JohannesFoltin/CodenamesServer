4d97974001963a03f0faf225aadd278d8664bbcd
Johannes <Johannes.Foltin@gmx.de>
2022-07-12 13:59:59 +0200

    added Select Oportunity added Annonce Oppertunity cleaned code

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module example/Go-Projekts/CodenamesServer
 
 go 1.18
 
-require github.com/gorilla/websocket v1.5.0
+require (
+	github.com/emirpasic/gods v1.18.1
+	github.com/gorilla/websocket v1.5.0
+)
--- main.go
+++ main.go
@@ -8,19 +8,9 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/emirpasic/gods/sets/hashset"
 	"github.com/gorilla/websocket"
 )
-
-// We'll need to define an Upgrader
-// this will require a Read and Write buffer size
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
-var isBlueGC bool
-var isRedGC bool
-var game Game
 
 //Typen: GCB,GCR,AgentB,AgentR
 type User struct {
@@ -47,14 +37,31 @@ type Game struct {
 	Users       []User
 }
 
-type Message struct{
-    Goal string
-    ParamOne string
-    ParamTwo int
+type Message struct {
+	Goal     string
+	ParamOne string
+	ParamTwo int
+}
+
+var (
+    game Game
+    isBlueGC bool = false
+    isRedGC bool = false
+)
+
+// & Referenz mitgeben (Pointer erstellen)
+// * wieder nur ein Element drauß machen
+
+func main() {
+	fmt.Println("Hello World")
+	gameInit()
+	http.HandleFunc("/ws", wsEndpoint)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func gameInit() {
 	startingPick := false
+    rand.Seed(time.Now().UnixNano())
 	if rand.Intn(2) == 0 {
 		startingPick = true
 	}
@@ -89,37 +96,7 @@ func gameInit() {
 	game.Cards = cards
 }
 
-func cardSelection(u User, c string) {
-	for i := 0; i < len(game.Users); i++ {
-		if u == game.Users[i] {
-			game.Users[i].Selected = c
-		}
-	}
-}
 
-func remove(s []string, i int) []string {
-	s[i] = s[len(s)-1]
-	return s[:len(s)-1]
-}
-
-func searchSlice(slice []string, s string) int {
-	tmp := -1
-	for i := 0; i < len(s); i++ {
-		if slice[i] == s {
-			tmp = i
-		}
-	}
-	return tmp
-}
-func broadcastGameState() {
-	tmp, err := json.Marshal(game)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for i := 0; i < len(game.Users); i++ {
-		game.Users[i].ws.WriteMessage(1, []byte(tmp))
-	}
-}
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 
 	//checkt die Herkunft von Clients die sich verbinden wollen. Return true, da wir nicht wählerisch sind. :)
@@ -146,7 +123,6 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 
 	reader(&game.Users[len(game.Users)-1])
 }
-
 func reader(u *User) {
 	init := false
 	for {
@@ -163,49 +139,152 @@ func reader(u *User) {
 
 		if !init {
 			//Verification JSON: {"name": "Johannes","typ": "AgentB"}
-			if tmpS[0] != '{' {
-				u.ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(1003, "Unsupported Data. Please send Verification JSON"))
-			} else {
-				if (isBlueGC && u.Typ == "GCB") || (isRedGC && u.Typ == "GCR") {
-					u.ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(1003, "This Type of Player is already taken. Please choose another one!"))
-				}
-				err := json.Unmarshal(p, u)
-				fmt.Println(err)
-				init = true
-				fmt.Println(u.Name)
-			}
-		} else {
-            tmpMssg := Message{"","",0}
-			err := json.Unmarshal(p,&tmpMssg)
+            err := json.Unmarshal(p, u)
             if err != nil {
                 fmt.Println(err)
             }
             switch{
-            case tmpMssg.Goal == "Get":
-                broadcastGameState()
-            case tmpMssg.Goal == "Select":
-                cardSelection(*u,tmpMssg.ParamOne)
-            case tmpMssg.Goal == "Announce":
-
-                game.Code = tmpMssg.ParamOne
-                game.Picks = tmpMssg.ParamTwo
+            case (isBlueGC && u.Typ == "GCB") || (isRedGC && u.Typ == "GCR"):
+                u.ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(1003, "This Type of Player is already taken. Please choose another one!"))
+                game.Users = remove(game.Users,*u)
+            case u.Typ == "GCB":
+                isBlueGC = true
+            case u.Typ == "GCR":
+                isRedGC = true 
             }
+			init = true
+			fmt.Println(u.Name)
+            broadcastGameState()
+
+		} else {
+			tmpMssg := Message{"", "", 0}
+			err := json.Unmarshal(p, &tmpMssg)
+			if err != nil {
+				fmt.Println(err)
+			}
+			switch {
+			case tmpMssg.Goal == "GET":
+				broadcastGameState()
+			case tmpMssg.Goal == "Select":
+                if u.Typ == "GCR" || u.Typ == "GCB" {
+                    break
+                }
+				if isAllowed(*u) {
+					cardSelection(*u, tmpMssg.ParamOne)
+					checkSelection()
+					broadcastGameState()
+				}
+			case tmpMssg.Goal == "Announce":
+				if isAllowed(*u) {
+					game.Code = tmpMssg.ParamOne
+					game.Picks = tmpMssg.ParamTwo
+				}
+			}
 
 		}
 
 	}
 }
 
-// & Referenz mitgeben (Pointer erstellen)
-// * wieder nur ein Element drauß machen
-
-func main() {
-	fmt.Println("Hello World")
-	gameInit()
-	//For "better" Random :)
-	rand.Seed(time.Now().UnixNano())
-	http.HandleFunc("/ws", wsEndpoint)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func isAllowed(u User) bool {
+	tmpBool := false
+	if u.Typ == "AgentB" && game.CurrentTeam {
+		tmpBool = true
+	}
+	if u.Typ == "AgentR" && !game.CurrentTeam {
+		tmpBool = true
+	}
+	if u.Typ == "GCB" && game.CurrentTeam {
+		tmpBool = true
+	}
+	if u.Typ == "GCR" && !game.CurrentTeam {
+		tmpBool = true
+	}
+	return tmpBool
 }
+
+func cardSelection(u User, c string) {
+	for i := 0; i < len(game.Users); i++ {
+		if u == game.Users[i] {
+			game.Users[i].Selected = c
+		}
+	}
+}
+
+func checkSelection() {
+	rightUserIndex := make([]int, 0)
+	for i, v := range game.Users {
+		if game.CurrentTeam {
+			if v.Typ == "AgentB" {
+				rightUserIndex = append(rightUserIndex, i)
+				fmt.Println(i)
+			}
+		}
+		if !game.CurrentTeam {
+			if v.Typ == "AgentR" {
+				rightUserIndex = append(rightUserIndex, i)
+				fmt.Println(i)
+			}
+		}
+	}
+	set := hashset.New()
+	for _, val := range rightUserIndex {
+		set.Add(game.Users[val].Selected)
+	}
+	s := set.Values()
+	fmt.Println(s[0])
+	if set.Size() == 1 && !set.Contains("") {
+		if set.Contains("Pass") {
+			deselectAllUsers()
+			game.CurrentTeam = !game.CurrentTeam
+			game.Code = ""
+			game.Picks = 0
+		} else {
+			for ind, vr := range game.Cards {
+				if s[0] == vr.Word {
+					fmt.Println("nice " + vr.Word)
+					game.Cards[ind].Coverd = false
+				}
+			}
+			deselectAllUsers()
+		}
+	}
+
+}
+
+func deselectAllUsers() {
+	for i := range game.Users {
+		game.Users[i].Selected = ""
+	}
+}
+
+func remove(s []User, u User) []User {
+    i := -1
+    for j,vel := range s{
+        if vel == u{
+            i = j
+        }
+    }
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+// We'll need to define an Upgrader
+// this will require a Read and Write buffer size
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func broadcastGameState() {
+	tmp, err := json.Marshal(game)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for i := range game.Users {
+		game.Users[i].ws.WriteMessage(1, []byte(tmp))
+	}
+}
+
 
 var words = []string{"Melone", "Laster", "Peitsche", "Berliner", "Wal", "Leiter", "Hubschrauber", "Dame", "Horn", "Platte", "Forscher", "Hering", "Nacht", "Stift", "Apfel", "Mangel", "Barren", "Kiwi", "Soldat", "Bach", "Mikroskop", "Erde", "Kohle", "Mund", "Loge", "Kreuz", "Chor", "Wald", "Meer", "Flügel", "Staat", "Futter", "Pistole", "Abgabe", "Flöte", "Kunde", "England", "Pension", "Umzug", "Lehrer", "Bett", "Viertel", "Wand", "Rute", "Tempo", "Elfenbein", "Seite", "Aufzug", "Prinzessin", "Bar", "Welle", "Film", "Kamm", "Stern", "Blatt", "Haupt", "Europa", "Schneemann", "Watt", "Loch", "Lösung", "Shakespeare", "Schloss", "Birne", "Fisch", "Bauer", "Mutter", "Netz", "Gang", "Gift", "Strom", "Absatz", "Satz", "Löwe", "Bulle", "Turm", "Blinker", "Ketchup", "Mine", "Engel", "Wasser", "Niete", "Kanal", "Maler", "Indien", "Mittel", "Bär", "Dietrich", "Rasen", "Hollywood", "Pilot", "Schelle", "Mini", "Fleck", "Note", "Maschine", "Erika", "Burg", "Mandel", "Pol", "Drucker", "Karotte", "Tisch", "Star", "Schnabeltier", "Brand", "Gericht", "Schnur", "Rost", "New York", "Schiff", "Stempel", "Scheibe", "Hund", "Nuss", "Fackel", "Gut", "Knopf", "Eis", "Börse", "Teleskop", "Römer", "Busch", "Ring", "Akt", "Grad", "Honig", "Frankreich", "Pass", "Verband", "Boxer", "Linie", "Wanze", "Zahn", "Feige", "Auto", "Australien", "Mühle", "Auflauf", "Dichtung", "Takt", "Gold", "Kreis", "Harz", "Raute", "Zoll", "Linse", "Limousine", "Dinosaurier", "Optik", "Demo", "Stuhl", "Kirche", "Bremse", "Zelle", "Brötchen", "Brücke", "Pfeife", "Daumen", "Leuchte", "Rücken", "Ball", "Mast", "König", "Käfer", "Glas", "Flur", "Hand", "Spiel", "Washington", "Steuer", "Ninja", "Krebs", "Rom", "Messe", "Zentaur", "Decke", "Herz", "Essen", "Hupe", "Funken", "Maus", "Orange", "Atlantis", "Ritter", "Chemie", "Ton", "Superheld", "Kraft", "Luxemburg", "Pyramide", "Vorsatz", "Gabel", "Fliege", "Pinguin", "Siegel", "Koks", "Roboter", "Krieg", "Alpen", "Fall", "Strand", "Stock", "Koch", "Hut", "Fläche", "Jet", "Inka", "Tokio", "Toast", "Messer", "Bock", "art", "Spinne", "Adler", "Antarktis", "Känguruh", "Pflaster", "Bein", "Krone", "Loch Ness", "Konzert", "Tafel", "Spion", "Strauß", "Wind", "Weide", "Moskau", "Griechenland", "Rock", "Finger", "Flasche", "Bahn", "Krankheit", "Fest", "Tor", "Kiefer", "Schirm", "Fallschirm", "Stamm", "Geschirr", "Papier", "Schein", "Fuß", "Kapitän", "China", "Uhr", "Golf", "Nagel", "Bayern", "Schalter", "Pferd", "Kiel", "Feuer", "Riemen", "Punkt", "Kippe", "Geist", "Quelle", "Bau", "Reif", "Korb", "Schule", "Katze", "Gürtel", "Hahn", "Riegel", "Tag", "Hotel", "Jura", "Glocke", "Horst", "Drossel", "Anwalt", "Wolkenkratzer", "Verein", "Königin", "Bogen", "Skelett", "Mal", "Torte", "Dieb", "Roulette", "Osten", "Ägypten", "Zylinder", "Karte", "Figur", "London", "Muschel", "Luft", "Kater", "Doktor", "Strudel", "Hase", "Matte", "Bombe", "Winnetou", "Boot", "Laser", "Pirat", "Straße", "Botschaft", "Moos", "Deutschland", "Löffel", "Millionär", "Becken", "Peking", "Schokolade", "Schlange", "Schnee", "Einhorn", "Läufer", "Bank", "Kerze", "Amerikaner", "Schuppen", "Fessel", "Jahr", "Korn", "Tod", "Mond", "Landung", "Oper", "Tau", "Stadion", "Arm", "Wurm", "Knie", "Lakritze", "Fuchs", "Zug", "Taste", "Satellit", "Lippe", "Sekretär", "Tanz", "Genie", "Quartett", "Lager", "Gesicht", "Drache", "Afrika", "Rolle", "Öl", "Leben", "Ente", "Würfel", "Morgenstern", "Batterie", "Oktopus", "Jäger", "Zeit", "Kapelle", "Zitrone", "Leim", "Schale", "Blüte", "Geschoss", "Feder", "Saturn", "Po", "Theater", "Hexe", "Bund", "Schotten", "Glück", "Atlas", "Polizei", "Hamburger", "Alien", "Grund", "Auge", "Olymp", "Schild", "Schuh", "Schimmel", "Blau", "Ausdruck", "Gras", "Wirtschaft", "Taucher", "Iris", "Bergsteiger", "Elf", "Riese", "Mark", "Brause", "Himalaja", "Gehalt", "Zwerg", "Bande", "Krankenhaus", "Wurf", "Bindung", "Heide", "Nadel", "Kasino", "Mexiko"}
